59eb9a1cd528b8967e417d88f7272929b18774fb
Stein-Bjarne <steinbjarne.johansen@difi.no>
2016-08-17 10:39:51 +0200

    Key name for push to github for metadata-validator

--- init.sh
+++ init.sh
@@ -2,7 +2,7 @@
 
 cp /tmp/git_key $JENKINS_HOME/git_key
 cp /tmp/docker_key $JENKINS_HOME/.docker/key.pem
-cp /tmp/id_rsa_metadata_validator $JENKINS_HOME/id_rsa_metadata_validator
+cp /tmp/id_rsa_metadata_validator $JENKINS_HOME/key_saml_metadata_validator
 
 addgroup -g ${gid} jenkins && adduser -h "$JENKINS_HOME" -u ${uid} -G jenkins -s /bin/bash -D jenkins
 chown -R jenkins:jenkins $JENKINS_HOME
