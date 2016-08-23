3f747bcd5167d9c053416a4b291d3002e85e23e0
Stein-Bjarne <steinbjarne.johansen@difi.no>
2016-08-23 16:20:25 +0200

    Småfiks

--- init.sh
+++ init.sh
@@ -2,7 +2,7 @@
 
 cp /tmp/git_key $JENKINS_HOME/git_key
 cp /tmp/docker_key $JENKINS_HOME/.docker/key.pem
-cp /tmp/key_saml_metadata_validator $JENKINS_HOME/github-buildkey
+cp /tmp/github-buildkey $JENKINS_HOME/github-buildkey
 
 addgroup -g ${gid} jenkins && adduser -h "$JENKINS_HOME" -u ${uid} -G jenkins -s /bin/bash -D jenkins
 chown -R jenkins:jenkins $JENKINS_HOME
