7e4fe39b1ed3be3fe103449205568925d52c0176
Stein-Bjarne <steinbjarne.johansen@difi.no>
2017-05-19 12:49:30 +0200

    feature/PBLEID-14064: Legge inn repo for idporten-app-dpi-reklame

--- files/init.sh
+++ files/init.sh
@@ -45,6 +45,8 @@ createJob eid-resilience git@git.difi.local:eid-common-resilience.git
 createJob eid-common git@git.difi.local:eid-common.git
 createJob idporten git@git.difi.local:idporten.git
 createJob idporten-authlevel git@git.difi.local:idporten-authlevel.git
+createJob idporten-app-dpi-reklame git@git.difi.local:idporten-app-dpi-reklame.git
+createJob idporten-minid-updater git@git.difi.local:idporten-minid-updater.git
 createJob eid-level1-poc git@git.difi.local:eid-level1-poc.git
 createJobNotTriggered puppet-hiera git@eid-gitlab.dmz.local:puppet/puppet_hiera.git
 createJobNotTriggered puppet-control git@eid-gitlab.dmz.local:puppet/puppet_control.git
