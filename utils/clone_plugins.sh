6d1b083b68aef84322f8f759cb81ecee5566fa4b
Erik Stedje <est@difi.no>
2017-08-11 10:28:54 +0000

    feature/PBLEID-14594: oppdatere jenkins

--- utils/clone_plugins.sh
+++ utils/clone_plugins.sh
@@ -6,9 +6,9 @@ exit 0
 fi
 PLUGINS=$(curl -s "${CLONE_URL}/pluginManager/api/xml?depth=1&xpath=/*/*/shortName|/*/*/version&wrapper=plugins")
 PLUGINS_RUN=$(echo $PLUGINS|perl -pe 's/.*?<shortName>([\w-]+).*?<version>([^<]+)()(<\/\w+>)+/RUN install-plugin.sh \1 \2;/g')
-grep -v "RUN install-plugin.sh" Dockerfile>tmp_Dockerfile
-sed -i  "s/^EXPOSE 8080/\\n${PLUGINS_RUN}\nEXPOSE 8080/g" tmp_Dockerfile
-sed -i  "s/\;RUN/\\nRUN/g" tmp_Dockerfile
-sed -i  "s/\;$/\\n/g" tmp_Dockerfile
-mv tmp_Dockerfile Dockerfile
+
+grep -v "RUN install-plugin.sh" ../docker/jenkins-plugins/Dockerfile>tmp_Dockerfile
+sed -i  "s/VOLUME/${PLUGINS_RUN}VOLUME/g" tmp_Dockerfile
+sed -i  "s/\;/\n/g" tmp_Dockerfile
+mv tmp_Dockerfile ../docker/jenkins-plugins/Dockerfile
 
