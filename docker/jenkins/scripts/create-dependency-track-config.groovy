4d3afb8dad6f212b28b88976a066cc6ea703a30e
Erik Stedje <Erik.Stedje@difi.no>
2019-07-09 13:48:09 +0000

    2019-07-09-1348-58fd172|PBLEID-18515: dependency_track inn i jenkins-docker take2

--- docker/jenkins/scripts/create-dependency-track-config.groovy
+++ docker/jenkins/scripts/create-dependency-track-config.groovy
@@ -5,12 +5,12 @@ import org.yaml.snakeyaml.Yaml
 import java.nio.file.Path
 import java.nio.file.Paths
 
-File templateFile = this.args[1] as File
+File templateFile = this.args[0] as File
 Map binding = [
         'dependency-track-api': new File('/run/secrets/dependency-track-api').text.trim(),
 ]
 Path homeDir = Paths.get(System.getenv("JENKINS_HOME"))
-println "Creating Jira configuration..."
+println "Creating Dependency-track configuration..."
 File outputFile = new File(homeDir.toString(), 'org.jenkinsci.plugins.DependencyTrack.DependencyTrackPublisher.xml')
 outputFile.delete()
 outputFile << new SimpleTemplateEngine().createTemplate(templateFile).make(binding).toString()
