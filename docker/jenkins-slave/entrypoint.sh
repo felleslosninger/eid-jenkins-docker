07cc99d361482b3cdb1da11a6838c4e9437fbd63
Randi Øyri <randi.oyri@digdir.no>
2021-09-01 11:47:17 +0000

    2021-09-01-1147-fa37c45|PBLEID-22339: Flytte over jenkins-pipeline frå github.com/difi til /felleslosninger

--- docker/jenkins-slave/entrypoint.sh
+++ docker/jenkins-slave/entrypoint.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
 curl -fsS --retry 100 --retry-delay 5 http://jenkins:8080/jnlpJars/agent.jar -o /tmp/agent.jar || exit 1
-mkdir ~/.ssh ||
+mkdir ~/.ssh
 groovy /files/create-ssh-known-hosts /config.yaml || exit 1
 exec java -jar /tmp/agent.jar -jnlpUrl http://jenkins:8080/computer/${SLAVE_NAME#*_}/slave-agent.jnlp -workDir "/tmp"
